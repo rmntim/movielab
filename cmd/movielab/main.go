e44aacfed0bd59a47448a52d54c5b7c67ffecbbf
Roman Timoshkin <rmntim@yandex.ru>
2024-03-17 11:46:05 +0300

    feat: add logger initialization

--- cmd/movielab/main.go
+++ cmd/movielab/main.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"github.com/rmntim/movielab/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
@@ -9,10 +16,26 @@ func main() {
 	cfg := config.MustLoad()
 
 	// init logger
+	logger := setupLogger(cfg.Env)
+
+	logger.Info("Starting server", slog.String("env", cfg.Env))
+	logger.Debug("Debug messages are enabled")
 
 	// init storage
 
 	// init router
 
 	// start server
+}
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
