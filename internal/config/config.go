d44a9ed52b72e11e409af6236764b2a68b0819eb
Roman Timoshkin <rmntim@yandex.ru>
2024-03-17 19:48:10 +0300

    refactor: change config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Config struct {
-	Env        string           `yaml:"env" env-required:"true"`
-	DBUrl      string           `yaml:"db_url" env:"DATABASE_URL" env-required:"true"`
-	HTTPServer HTTPServerConfig `yaml:"http_server"`
+	Env              string `yaml:"env" env-required:"true"`
+	DBUrl            string `yaml:"db_url" env:"DATABASE_URL" env-required:"true"`
+	HTTPServerConfig `yaml:"http_server"`
 }
 
 type HTTPServerConfig struct {
